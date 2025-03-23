17b25031926caff4f72dccfa554f9be8ec13d5d5
ihor shyshov <sp.ukr.rs@gmail.com>
2025-03-23 23:09:01 +0200

    Add loop

--- Smart Calculator (Go)/task/main.go
+++ Smart Calculator (Go)/task/main.go
@@ -1,13 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	var a, b int
-	_, err := fmt.Scan(&a, &b)
-	if err != nil {
-		fmt.Println("Invalid input")
-		return
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if input == "/exit" {
+			fmt.Println("Bye!")
+			break
+		}
+
+		if input == "" {
+			continue
+		}
+
+		parts := strings.Fields(input)
+		if len(parts) == 1 {
+			num, err := strconv.Atoi(parts[0])
+			if err != nil {
+				fmt.Println("Invalid input")
+				continue
+			}
+			fmt.Println(num)
+		} else if len(parts) == 2 {
+			num1, err1 := strconv.Atoi(parts[0])
+			num2, err2 := strconv.Atoi(parts[1])
+			if err1 != nil || err2 != nil {
+				fmt.Println("Invalid input")
+				continue
+			}
+			fmt.Println(num1 + num2)
+		} else {
+			fmt.Println("Invalid input")
+		}
 	}
-	fmt.Println(a + b)
 }
